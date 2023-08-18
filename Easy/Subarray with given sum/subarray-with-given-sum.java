2bd9c39163326baf9b959453c74f1ec6cfbbaffb
Ankit Pandey <88959758+ankitiitm1@users.noreply.github.com>
2023-08-18 21:28:00 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Subarray with given sum/subarray-with-given-sum.java
@@ -0,0 +1,78 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main{
+	static BufferedReader br;
+    static PrintWriter ot;
+    public static void main(String[] args) throws IOException{
+        
+        br = new BufferedReader(new InputStreamReader(System.in));
+        ot = new PrintWriter(System.out);
+
+        int t = Integer.parseInt(br.readLine());
+
+        while(t-->0){
+            
+            String s[] = br.readLine().trim().split(" ");
+            
+            int n = Integer.parseInt(s[0]);
+            int k = Integer.parseInt(s[1]);
+            int a[] = new int[n];
+            s = br.readLine().trim().split(" ");
+            for(int i = 0; i < n; i++)
+                a[i] = Integer.parseInt(s[i]);
+            Solution obj = new Solution();
+            ArrayList<Integer> res = obj.subarraySum(a, n, k);
+            for(int ii = 0;ii<res.size();ii++)
+                ot.print(res.get(ii) + " ");
+            ot.println();
+        }
+        ot.close();
+    }
+
+}
+// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to find a continuous sub-array which adds up to a given number.
+    static ArrayList<Integer> subarraySum(int[] arr, int n, int s) 
+    {
+        // Your code here
+        
+       
+        ArrayList<Integer> arr1=new ArrayList();
+         if(s==0){
+            arr1.add(-1);
+        return arr1;
+        }
+        int start=0;
+        int currSum=arr[0];
+        int end=0;
+        while(end<n){
+            if(currSum==s){
+                arr1.add(start+1);
+                arr1.add(end+1);
+                return arr1;
+            }
+            if(currSum<s){
+                end++;
+                if(end<n){
+                    currSum+=arr[end];
+                }
+            }
+            else if(currSum>s){
+                currSum-=arr[start];
+                start++;
+            }
+            
+        }
+        arr1.add(-1);
+        return arr1;
+        
+        
+    }
+}
