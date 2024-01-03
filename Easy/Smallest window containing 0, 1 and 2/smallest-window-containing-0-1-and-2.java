9d866f38b2ffca046cccd194c2d7cdafce2d21bf
Ankit Pandey <88959758+ankitiitm1@users.noreply.github.com>
2024-01-03 22:35:11 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Smallest window containing 0, 1 and 2/smallest-window-containing-0-1-and-2.java
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String S = read.readLine();
+            Solution ob = new Solution();
+            int ans = ob.smallestSubstring(S);
+
+            System.out.println(ans);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int smallestSubstring(String S) {
+        int zero=-1,one=-1,two=-1,res=100005;
+        
+        for(int i=0;i<S.length();i++){
+            if(S.charAt(i)=='0') zero=i;
+            if(S.charAt(i)=='1') one=i;
+            if(S.charAt(i)=='2') two=i;
+            
+            if(zero==-1 || one==-1 || two==-1) continue;
+            int min=Math.min(Math.min(zero,one),two);
+            int max=Math.max(Math.max(zero,one),two);
+
+            res=Math.min(res,max-min+1);
+
+        }
+        
+        return res==100005?-1:res;
+    }
+};
